f1df4f27679368c292204110e41d8583042a7c34
Lasitha <lasithamarasinghe1992@gmail.com>
2019-10-07 15:46:25 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    
+    $('.wpentry1').waypoint(function (direction) {
+        $('.wpentry1').addClass('animated fadeIn');
+    }, {
+        offset: '70%'
+    })
+    $('.wpentry2l').waypoint(function (direction) {
+        $('.wpentry2l').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    })
+    $('.wpentry2r').waypoint(function (direction) {
+        $('.wpentry2r').addClass('animated fadeInRight');
+    }, {
+        offset: '75%'
+    })
+    $('.wpentry3l').waypoint(function (direction) {
+        $('.wpentry3l').addClass('animated fadeInLeft');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry3r').waypoint(function (direction) {
+        $('.wpentry3r').addClass('animated fadeInRight');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry4l').waypoint(function (direction) {
+        $('.wpentry4l').addClass('animated fadeInLeft');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry4r').waypoint(function (direction) {
+        $('.wpentry4r').addClass('animated fadeInRight');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry5').waypoint(function (direction) {
+        $('.wpentry5').addClass('animated zoomIn');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry6').waypoint(function (direction) {
+        $('.wpentry6').addClass('animated zoomIn');
+    }, {
+        offset: '80%'
+    })
+    $('.wpentry7').waypoint(function (direction) {
+        $('.wpentry7').addClass('animated zoomIn');
+    }, {
+        offset: '70%'
+    })
+    $('.wpentry8').waypoint(function (direction) {
+        $('.wpentry8').addClass('animated zoomIn');
+    }, {
+        offset: '70%'
+    })
+    
+
+
+
+});
+
